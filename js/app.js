2b966bef72745c9c3aa6c705c285438aac4aa096
tomi <tomi>
2016-12-01 15:26:47 +0100

    -added card layout

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ function newItem(){
     var ul = document.getElementById('list');
     var li = document.createElement('li');
 
-    li.appendChild(document.createTextNode('- '+item));
+    li.className = "todo-element";
+    li.appendChild(document.createTextNode(item));
     ul.appendChild(li);
     document.getElementById('input').value ="";
     li.onclick = removeItem;
